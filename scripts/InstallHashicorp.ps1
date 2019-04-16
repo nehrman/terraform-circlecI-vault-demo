3da22da622f6126d3b846c212c85819479e70df7
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2019-04-16 20:02:48 +0200

    Update InstallHashicorp.ps1

--- scripts/InstallHashicorp.ps1
+++ scripts/InstallHashicorp.ps1
@@ -1,15 +1,15 @@
 
 Import-Module BitsTransfer
-Start-BitsTransfer -Source https://releases.hashicorp.com/consul/1.4.4+ent/consul_1.4.4+ent_windows_amd64.zip -Destination C:\Users\$env:username\Downloads\consul_1.4.4+ent_windows_amd64.zip
+Start-BitsTransfer -Source https://releases.hashicorp.com/consul/1.4.4+ent/consul_1.4.4+ent_windows_amd64.zip -Destination C:\Users\nicolas\Downloads\consul_1.4.4+ent_windows_amd64.zip
 
-Expand-Archive C:\Users\$env:username\Downloads\consul_1.4.4+ent_windows_amd64.zip -DestinationPath C:\Hashicorp\Consul\
+Expand-Archive C:\Users\nicolas\Downloads\consul_1.4.4+ent_windows_amd64.zip -DestinationPath C:\Hashicorp\Consul\
 
 sc.exe create "Hashicorp Consul" binPath= "C:\Hashicorp\Consul\consul.exe agent -config-file=C:\Hashicorp\Consul\config.json" start= auto
 
 sc.exe start "Hashicorp Consul" 
 
-Start-BitsTransfer -Source https://releases.hashicorp.com/nomad/0.9.0/nomad_0.9.0_windows_amd64.zip -Destination C:\Users\$env:username\Downloads\nomad_0.9.0_windows_amd64.zip
-Expand-Archive C:\Users\$env:username\Downloads\nomad_0.9.0_windows_amd64.zip -DestinationPath C:\Hashicorp\Nomad\
+Start-BitsTransfer -Source https://releases.hashicorp.com/nomad/0.9.0/nomad_0.9.0_windows_amd64.zip -Destination C:\Users\nicolas\Downloads\nomad_0.9.0_windows_amd64.zip
+Expand-Archive C:\Users\nicolas\Downloads\nomad_0.9.0_windows_amd64.zip -DestinationPath C:\Hashicorp\Nomad\
 
 sc.exe create "Hashicorp Nomad" binPath= "C:\Hashicorp\Nomad\nomad.exe agent -config-file=C:\Hashicorp\Nomad\config.json" start= auto
 
