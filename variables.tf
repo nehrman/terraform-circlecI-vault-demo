b19878770289c613e46cf41db77ff09efcda3dfb
nehrman <nicolas.ehrman@gmail.com>
2019-08-07 10:59:00 +0200

    Modifying variable file

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 ##############################################################################
-# Variables File
+# Variables Files
 # 
 # Here is where we store the default values for all the variables used in our
 # Terraform code. If you create a variable with no default, the user will be
