7ed737e59d541105bf7cf8a481ad8e725cb2b07d
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2019-09-16 11:00:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "rootcertificate" {
   source                = "github.com/GuyBarros/terraform-tls-certificate"
-  version               = "0.0.1"
+#  version               = "0.0.1"
   algorithm             = "ECDSA"
   ecdsa_curve           = "P521"
   common_name           = "service.consul"
