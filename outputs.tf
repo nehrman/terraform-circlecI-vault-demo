5f934bfae9156c6238640c158187e60a780159c0
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2019-09-16 12:02:34 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,42 @@
 # providing instructions to the user on how to connect to their own custom 
 # demo environment.
 
-output "file" {
-  value = "${data.template_file.consulconfig.rendered}"
+#output "file" {
+#  value = "${data.template_file.consulconfig.rendered}"
+#}
+
+output "rg_name" {
+  value = "${azurerm_resource_group.windows-rg.name}"
+  description = "Name of the resource group created by terraform"
 }
+
+output "vnet_name" {
+  value = "${azurerm_virtual_network.windows-vnet.name}"
+  description = "Name of the Virtual Network created by terraform"
+}
+
+output "vnet_address_space" {
+  value = "${azurerm_virtual_network.windows-vnet.address_space}"
+  description = "Address Space of the Virtual Network created by terraform"
+}
+
+output "subnet_name" {
+  value = "${azurerm_subnet.windows-subnet.name}"
+  description = "Name of the Subnet created by terraform"
+}
+
+output "subnet_address_prefix" {
+  value = "${azurerm_subnet.windows-subnet.address_prefix}"
+  description = "Address Prefix of the Subnet created by terraform"
+}
+
+output "vm_name" {
+  value = "${azurerm_virtual_machine[count.index].web_server.name}"
+  description = "Name of Virtual Machines created by terraform"
+}
+
+output "vm_blic_ipu" {
+  value = "${azurerm_public_ip[count.index].windows-pip.ip_address}"
+  description = "Public IP Addresses of Virtual Machines created by terraform"
+}
+  
